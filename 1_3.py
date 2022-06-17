4c6880153f95fd735c158bf5e68759f78f0c69b6
Khristofor Kelekhsaev <consulmad@gmail.com>
2022-06-17 12:45:55 +0300

    домашнаяя работа 1

---
+++ 1_3.py
@@ -0,0 +1,7 @@
+
+n = int(input('enter a number n: '))
+
+m = n ** 2
+f = n ** 3
+
+print(n + m + f)
---
+++ 1_4.py
@@ -0,0 +1,30 @@
+num = int(input('enter a number:  '))
+
+i = 0
+
+while True:
+    n = num % 10
+    if i < n:
+        i = n
+    else:
+        i = i
+        num = num // 10
+        if num < 1:
+            break
+print(i)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
